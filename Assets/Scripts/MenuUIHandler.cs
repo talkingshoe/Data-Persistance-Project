d6d66e911103c01bb69981cd11fa281ba59f237a
talkingshoe <cianomatthew@gmail.com>
2024-11-10 12:50:48 +0200

    completed
    implemented walkthrough from fellow student

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+using TMPro;
+public class MenuUIHandler : MonoBehaviour
+{
+    //This is the handler of the main menu scene
+
+    [SerializeField] TMP_Text PlayerNameInput;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetPlayerName()
+    {
+        PlayerDataHandle.Instance.PlayerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
